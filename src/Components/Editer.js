5ddda339c34425929bcce57c46f91e6f858432c9
Mk016 <mahendrakumawat80224@gmail.com>
2024-07-25 04:16:01 +0530

    a

--- src/Components/Editer.js
+++ src/Components/Editer.js
@@ -1,44 +1,47 @@
-import React , {useState} from 'react'
+import React, { useState } from 'react';
 
-import 'codemirror/lib/codemirror.css'
-import 'codemirror/theme/material.css'
-import 'codemirror/mode/xml/xml'
-import 'codemirror/mode/xml/xml'
-import 'codemirror/mode/css/css'
-import 'codemirror/mode/javascript/javascript'
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/material.css';
+import 'codemirror/mode/xml/xml';
+import 'codemirror/mode/css/css';
+import 'codemirror/mode/javascript/javascript';
 
-import {Controlled } from 'react-codemirror2'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCompressAlt,faExpandAlt} from '@fortawesome/free-solid-svg-icons'
+import { Controlled as ControlledEditor } from 'react-codemirror2';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCompressAlt, faExpandAlt } from '@fortawesome/free-solid-svg-icons';
 
-const Editor = ({ displayName , language , value, onChange }) => {
-    const [open , setOpen] = useState(true)
+const Editor = ({ displayName, language, value, onChange }) => {
+  const [open, setOpen] = useState(true);
 
-  function handleChange(editor, data, value){
-    onChange(value)
+  function handleChange(editor, data, value) {
+    onChange(value);
   }
+
   return (
-    <div className={`editor-container ${open ? "" : "collapsed"}`}>
-      <div className='editor-title'>
+    <div className={`editor-container ${open ? '' : 'collapsed'}`}>
+      <div className="editor-title">
         {displayName}
-        <button type='button' className='btn-styles' onClick={() => setOpen(prevOpen => !prevOpen)}><FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt} /></button>
-        
+        <button
+          type="button"
+          className="btn-styles"
+          onClick={() => setOpen(prevOpen => !prevOpen)}
+        >
+          <FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt} />
+        </button>
       </div>
-      <Controlled 
+      <ControlledEditor
         onBeforeChange={handleChange}
         value={value}
-        className='code-mirror-wrapper'
+        className="code-mirror-wrapper"
         options={{
-          lineWrapping:true,
-          lint:true,
-          theme:'material',
-          mode: language
+          lineWrapping: true,
+          lint: true,
+          theme: 'material',
+          mode: language,
         }}
-       
       />
-
     </div>
-  )
-}
+  );
+};
 
-export default Editor
+export default Editor;
