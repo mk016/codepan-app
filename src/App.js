aedbe896ab8f58f2b0a38e01a104be2734ca2e5b
Mk016 <mahendrakumawat80224@gmail.com>
2024-07-24 21:41:36 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 
 import './App.css';
 import Appbar from './Components/AppBar'
+import Editer from './Components/Editer'
 
 function App() {
   return (
 <>
 <Appbar />
+
+<div className='pane top-pane'> 
+    <Editer />
+    <Editer />
+    <Editer />
+</div>
+  <div className='pane'> 
+    <iframe title="output" sandbox='allow-scripts'
+    
+      frameBorder="0"
+      width="100%"
+      height="100%"
+    />
+
+</div>
 </>
   );
 }
