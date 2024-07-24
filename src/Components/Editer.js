dd1f4b5a31330711cfc7bb60aaca163fd6284ba6
Mk016 <mahendrakumawat80224@gmail.com>
2024-07-24 22:48:56 +0530

    a

--- src/Components/Editer.js
+++ src/Components/Editer.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React , {useState} from 'react'
+
 import 'codemirror/lib/codemirror.css'
 import 'codemirror/theme/material.css'
 import 'codemirror/mode/xml/xml'
@@ -9,14 +10,16 @@ import 'codemirror/mode/javascript/javascript'
 import {Controlled } from 'react-codemirror2'
 
 const Editor = ({ displayName , language , value, onChange }) => {
+    const [open , setOpen] = useState(true)
+
   function handleChange(editor, data, value){
     onChange(value)
   }
   return (
-    <div className='editor-container'>
+    <div className={`editor-container ${open ? "" : "collapsed"}`}>
       <div className='editor-title'>
         {displayName}
-        <button>O-C</button>
+        <button onClick={() => setOpen(prevOpen => !prevOpen)}>O-C</button>
       </div>
       <Controlled 
         onBeforeChange={handleChange}
