00b7db75bf55e2cd2e228e531fd4163cd5207309
Mk016 <mahendrakumawat80224@gmail.com>
2024-07-24 22:48:54 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,32 +1,42 @@
-
 import { useState } from 'react';
 import './App.css';
-import Appbar from './Components/AppBar'
-import Editer from './Components/Editer'
+import Appbar from './Components/AppBar';
+import Editer from './Components/Editer';
 
 function App() {
+  const [html, setHtml] = useState('');
+  const [css, setCss] = useState('');
+  const [js, setJs] = useState('');
 
-  const [html, setHtml] = useState('')
-  const [css, setcss] = useState('')
-  const [Js, setJs] = useState('')
+  const srcDoc = `
+    <html>
+      <body>${html}</body>
+      <style>${css}</style>
+      <script>${js}</script>
+    </html>
+  `;
+
   return (
-<>
-<Appbar />
+    <>
+      <Appbar />
 
-<div className='pane top-pane'> 
-    <Editer language="xml" displayName="HTML" value={html} onChange={setHtml} />
-    <Editer language="css" displayName="css" value={css} onChange={setcss}/>
-    <Editer language="javaScript" displayName="js" value={Js} onChange={setJs}/>
-</div>
-  <div className='pane'> 
-    <iframe title="output" sandbox='allow-scripts'
-      frameBorder={0}
-      width="100%"
-      height="100%"
-    />
-
-</div>
-</>
+      <div className="pane top-pane">
+        <Editer language="xml" displayName="HTML" value={html} onChange={setHtml} />
+        <Editer language="css" displayName="CSS" value={css} onChange={setCss} />
+        <Editer language="javascript" displayName="JS" value={js} onChange={setJs} />
+      </div>
+      
+      <div className="pane">
+        <iframe
+          srcDoc={srcDoc}
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder={0}
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </>
   );
 }
 
