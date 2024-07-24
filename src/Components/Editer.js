b1d46dd4983177bdc43bb72b86602b660593eb03
Mk016 <mahendrakumawat80224@gmail.com>
2024-07-24 22:54:08 +0530

    a

--- src/Components/Editer.js
+++ src/Components/Editer.js
@@ -8,6 +8,8 @@ import 'codemirror/mode/css/css'
 import 'codemirror/mode/javascript/javascript'
 
 import {Controlled } from 'react-codemirror2'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCompressAlt,faExpandAlt} from '@fortawesome/free-solid-svg-icons'
 
 const Editor = ({ displayName , language , value, onChange }) => {
     const [open , setOpen] = useState(true)
@@ -19,7 +21,8 @@ const Editor = ({ displayName , language , value, onChange }) => {
     <div className={`editor-container ${open ? "" : "collapsed"}`}>
       <div className='editor-title'>
         {displayName}
-        <button onClick={() => setOpen(prevOpen => !prevOpen)}>O-C</button>
+        <button type='button' className='btn-styles' onClick={() => setOpen(prevOpen => !prevOpen)}><FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt} /></button>
+        
       </div>
       <Controlled 
         onBeforeChange={handleChange}
